dc2bf211138b034cc351c609dcb8d09b5d3eacce
Guang Wei <guangwei.ngoh@gmail.com>
2020-08-07 03:24:46 +0800

    Implemented full game playout

--- Playout.py
+++ Playout.py
@@ -16,12 +16,19 @@ from PIL import Image
 class Playout(object):
 
     def __init__(self, board, games, depth, algo_obj):
+        self.starting_board_state = board
         self.board_state = board
         self.num_games = games
         self.search_depth = depth
         self.algo_obj = algo_obj
         globals()['boardholder'] = st.empty()
         self.animate_board()  # render starting board position
+        if board.turn:
+            self.original_player = True
+        else:
+            self.original_player = False
+        self.win_count = 0
+        self.lose_count = 0
 
     def animate_board(self):
         board_svg = chess.svg.board(board=self.board_state)
@@ -31,33 +38,68 @@ class Playout(object):
         globals()['boardholder'].image(boardimg)
 
     def run_algo_playout(self):
+        # initialize root node with children at depth 1
+        # update algo board state with current board
+        self.algo_obj.starting_board_state = self.board_state.copy()
+        root_node = self.algo_obj.algo_init()
+        if self.board_state.turn:
+            player = True
+        else:
+            player = False
+        start_time = datetime.datetime.utcnow()  # current time
+        # run simulation until allowed time is reached
+        while datetime.datetime.utcnow() - start_time < self.algo_obj.calc_time:
+            end_sim = self.algo_obj.algo(root_node, player)
+            if end_sim:
+                break
+        best_move, _, _, _, _, _ = self.algo_obj.algo_render()
         try:
-            while(1):
-                # initialize root node with children at depth 1
-                root_node = self.algo_obj.algo_init()
-                if self.board_state.turn:
-                    player = True
-                else:
-                    player = False
-                start_time = datetime.datetime.utcnow()  # current time
-                # run simulation until allowed time is reached
-                while datetime.datetime.utcnow() - start_time < self.algo_obj.calc_time:
-                    end_sim = self.algo_obj.algo(root_node, player)
-                    if end_sim:
-                        break
-                best_move, _, _, _, _, _ = self.algo_obj.algo_render()
-                self.board_state.push_san(best_move)
-                self.animate_board()
-
-                # print(datetime.datetime.utcnow())
-                opponent_engine = chess.engine.SimpleEngine.popen_uci(
-                    "stockfish.exe")
-                info = opponent_engine.analyse(
-                    self.board_state, chess.engine.Limit(depth=self.search_depth))
-                # print(datetime.datetime.utcnow())
-                self.board_state.push(info["pv"][0])
-                opponent_engine.quit()
-                time.sleep(1)
-                self.animate_board()
+            self.board_state.push_san(best_move)
         except:
-            opponent_engine.quit()
+            print(self.board_state.fen())
+            print(list(self.board_state.legal_moves))
+        self.animate_board()
+
+        if self.board_state.is_game_over():
+            if self.board_state.is_checkmate():  # assign winner only if checkmate
+                if self.board_state.turn == self.original_player:
+                    self.lose_count += 1
+                else:
+                    self.win_count += 1
+            return False  # stop game and reset
+
+        # print(datetime.datetime.utcnow())
+        opponent_engine = chess.engine.SimpleEngine.popen_uci(
+            "stockfish.exe")
+        # info = opponent_engine.analyse(
+        #     self.board_state, chess.engine.Limit(depth=self.search_depth))
+        info = opponent_engine.analyse(
+            self.board_state, chess.engine.Limit(time=0.005))
+        # print(datetime.datetime.utcnow())
+        try:
+            self.board_state.push(info["pv"][0])
+        except:
+            print(self.board_state.fen())
+            print(list(self.board_state.legal_moves))
+        opponent_engine.quit()
+        time.sleep(0.5)
+        self.animate_board()
+
+        if self.board_state.is_game_over():
+            if self.board_state.is_checkmate():  # assign winner only if checkmate
+                if self.board_state.turn == self.original_player:
+                    self.lose_count += 1
+                else:
+                    self.win_count += 1
+            return False  # stop game and reset
+
+        return True  # continue game
+
+    def iterate(self):
+        for i in range(self.num_games):
+            result = True
+            while(result):
+                result = self.run_algo_playout()
+                print('Wins: ' + str(self.win_count))
+                print('Loses: ' + str(self.lose_count))
+            self.board_state = self.starting_board_state.copy()
